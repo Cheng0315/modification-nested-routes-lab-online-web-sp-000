70bae23fe4e187f6db1399dc9ff8ca53cfc0ec96
Cheng Vue <csalvue@gmail.com>
2019-02-01 18:11:24 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :artists, only: [:index, :show, :edit, :new, :create, :destroy, :update] do
-    resources :songs, only: [:index, :show]
+    resources :songs, only: [:index, :show, :new, :edit]
   end
   resources :songs, only: [:index, :show, :edit, :new, :create, :destroy, :update]
 end
