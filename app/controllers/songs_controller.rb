26dd33a7d988a71f20ed71a932313d64bb43ce28
Cheng Vue <csalvue@gmail.com>
2019-02-01 18:34:41 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -48,7 +48,7 @@ class SongsController < ApplicationController
       if @author.nil?
         redirect_to artists_path
       else
-        @song = Songs.find_by(id: params[:id])
+        @song = Song.find_by(:id)
         redirect_to artists_path if @song.nil?
       end
     else
