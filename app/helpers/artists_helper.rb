ccc0f322dcd97a511a99d433344416e6e916c2f1
Cheng Vue <csalvue@gmail.com>
2019-02-01 19:40:48 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,7 +3,12 @@ module ArtistsHelper
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
-  def artist_select(artist_id, f)
-    f.select artist_id, options_from_collection_for_select(Artist.all, :id, :name)
+  def artist_select(song, f)
+    if song.artist.nil?
+      f.select :artist_id, options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      f.hidden_field :artist_id
+    end
+
   end
 end
